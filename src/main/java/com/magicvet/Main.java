8382ddede530a04cfa4ccdbee86851390094e1c5
UAndriiP <34newl@gmail.com>
2024-06-21 10:19:29 +0300

    req-03-02_test

--- src/main/java/com/magicvet/Main.java
+++ src/main/java/com/magicvet/Main.java
@@ -1,72 +1,16 @@
 package main.java.com.magicvet;
 
+import main.java.com.magicvet.component.ApplicationRunner;
+
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
 
 public class Main {
-    static String password = "default";
-    static Scanner scanner = new Scanner(System.in);
 
-    static String email_pattern = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+    public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        run();
-
-    }
-
-    static void run() {
-        if (auth()) {
-            registerNewClient();
-
-        }
-
-    }
-
-    static boolean auth() {
-        boolean accepted = false;
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Password: ");
-            String input = scanner.nextLine();
-            if (password.equals(input)) {
-                accepted = true;
-                break;
-            } else {
-                System.out.println("Access denied. Please check your password.");
-            }
-        }
-        System.out.println(accepted ? "Welcome to the Magic Vet!" : "Application hsa been blocked");
-        return accepted;
-    }
-
-    static void registerNewClient() {
-        System.out.println("Please provide client details.");
-        System.out.println("Email: ");
-        String email = scanner.nextLine();
-
-        if (inEmailValid(email)) {
-            Client client = buildClient(email);
-            System.out.println("New client; " + client.firstName + " " + client.lastName + " (" + client.email + ")");
-
-        } else {
-            System.out.println("Provided email is invalid");
-        }
-    }
-
-    static Client buildClient(String email) {
-        Client client=new Client();
-        client.email=email;
-
-        System.out.println("First name: ");
-        client.firstName= scanner.nextLine();
-        System.out.println("Last name: ");
-        client.lastName= scanner.nextLine();
-        return client;
-    }
-
-    static boolean inEmailValid(String email) {
-        Pattern pattern=Pattern.compile(email_pattern);
-        Matcher matcher= pattern.matcher(email);
-        return matcher.matches();
+        ApplicationRunner runner = new ApplicationRunner();
+        runner.run();
     }
 }
